2223f76f650b31038bcbbde390eb0e1939509d7e
Sergei Vasilevkin <svasilevkin@gmail.com>
2021-05-31 10:37:10 +0300

    Remove boiler plate template code.

---
+++ lib/app/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GitHubClientApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
